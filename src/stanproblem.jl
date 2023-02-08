378725c5569fbed73b32ddf2371c35f67d6894c9
Seth Axen <seth@sethaxen.com>
2023-02-08 10:46:08 +0100

    Note that models are unconstrained

--- src/stanproblem.jl
+++ src/stanproblem.jl
@@ -1,7 +1,7 @@
 """
     StanProblem(model::BridgeStan.StanModel; nan_on_error::Bool=false)
 
-A wrapper for a Stan model with data, implementing the
+A wrapper for an unconstrained Stan model with data, implementing the
 [LogDensityProblems](https://www.tamaspapp.eu/LogDensityProblems.jl/) interface.
 
 If `nan_on_error=true`, then any errors from Stan will be suppressed, and `NaN`s will be
