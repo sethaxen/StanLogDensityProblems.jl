12e8ae524985c19ec1a121a50d094adcc2352dff
Seth Axen <seth@sethaxen.com>
2025-03-31 18:00:45 +0200

    Use DocumenterInterLinks to link to BridgeStan (#21)

--- src/stanproblem.jl
+++ src/stanproblem.jl
@@ -17,11 +17,10 @@ end
 """
     StanProblem(lib::String[, data::String[ seed::Int]]; nan_on_error::Bool=false, kwargs...)
 
-Construct a `BridgeStan.StanModel` and wrap it as a `StanProblem`.
+Construct a [`BridgeStan.StanModel`](@extref) and wrap it as a `StanProblem`.
 
 `lib` is a path either to a compiled Stan model or to a `.stan` file. For details on the
-arguments, see the docstring for
-[`BridgeStan.StanModel`](https://roualdes.github.io/bridgestan/languages/julia.html#BridgeStan.StanModel).
+arguments, see the docstring for [`BridgeStan.StanModel`](@extref).
 
 !!! note
     By default, Stan does not compile the model with multithreading support. If this is
