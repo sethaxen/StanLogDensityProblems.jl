28b69a205c793392e72c49e7d24af88215f0885e
Seth Axen <seth@sethaxen.com>
2025-04-01 12:25:41 +0200

    Don't copy Stan file if it already exists (#22)
    * Add SHA as dependency
    
    * Auto-rearrange Project.toml sections
    
    * Don't copy Stan file if identical
    
    * Test Stan file is not copied if identical
    
    * Increment patch number
    
    * Factor file identity check into utility function
    
    * Correct docstring
    
    * Load SHA directly even with Requires
    
    * Add Requires to weakdeps
    
    * Make SHA a weakdep on Julia v1.9+

--- ext/StanLogDensityProblemsPosteriorDBExt.jl
+++ ext/StanLogDensityProblemsPosteriorDBExt.jl
@@ -7,6 +7,7 @@ if EXTENSIONS_SUPPORTED
 else  # using Requires
     using ..PosteriorDB: PosteriorDB
 end
+using SHA: SHA
 
 """
     StanProblem(
@@ -21,11 +22,11 @@ end
 Construct a `StanProblem` from the Stan model implementation and dataset corresponding to
 `posterior`.
 
-The model file will be copied to `path` before compilation. `force=true` will first remove
-an existing file. However, if the original file has already been compiled in this directory,
-the new model will not be compiled.
+The model file will be copied to `path` before compilation. If the file already exists in
+`path` and is not identical to the original file, it will be overwritten if `force=true`;
+otherwise, an error will be thrown.
 
-Remaining `args` and `kwargs` are forwarded to the main constructor.
+Remaining `args` and `kwargs` are forwarded to the main `StanProblem` constructor.
 """
 function StanLogDensityProblems.StanProblem(
     post::PosteriorDB.Posterior, path::AbstractString, args...; force::Bool=false, kwargs...
@@ -33,9 +34,15 @@ function StanLogDensityProblems.StanProblem(
     model = PosteriorDB.model(post)
     stan_file = PosteriorDB.path(PosteriorDB.implementation(model, "stan"))
     stan_file_new = joinpath(path, basename(stan_file))
-    cp(stan_file, stan_file_new; force=force)
+    if !isfile(stan_file_new) || !_is_file_identical(stan_file_new, stan_file)
+        cp(stan_file, stan_file_new; force=force)
+    end
     data = PosteriorDB.load(PosteriorDB.dataset(post), String)
     return StanLogDensityProblems.StanProblem(stan_file_new, data, args...; kwargs...)
 end
+
+_is_file_identical(f1::AbstractString, f2::AbstractString) = _hash(f1) == _hash(f2)
+
+_hash(file::AbstractString) = open(SHA.sha256, file; read=true)
 
 end  # module
--- test/posteriordb.jl
+++ test/posteriordb.jl
@@ -1,3 +1,4 @@
+using BridgeStan
 using LogDensityProblems
 using PosteriorDB
 using StanLogDensityProblems
@@ -8,14 +9,27 @@ using Test
     post = PosteriorDB.posterior(pdb, "dogs-dogs")
     @testset for nan_on_error in (false, true)
         path = mktempdir(; cleanup=false)
+        stan_file = joinpath(path, "dogs.stan")
         prob = StanProblem(post, path; nan_on_error=nan_on_error)
         @test prob isa StanProblem{<:BridgeStan.StanModel,nan_on_error}
-        @test isfile(joinpath(path, "dogs.stan"))
+        @test isfile(stan_file)
         dim = LogDensityProblems.dimension(prob)
         x = randn(dim)
         LogDensityProblems.logdensity(prob, x)
-        @test_throws ArgumentError StanProblem(post, path; nan_on_error=nan_on_error)
-        prob2 = StanProblem(post, path; force=true, nan_on_error=nan_on_error)
+
+        # Because Stan file has not changed, no error raised.
+        prob2 = StanProblem(post, path; nan_on_error=nan_on_error)
         LogDensityProblems.logdensity(prob2, x)
+
+        # Because file exists but is nonidentical, error raised.
+        # `cp` copies with the original permissions, so we can't modify the copy unless we
+        # change the permissions
+        chmod(stan_file, 0o644)
+        open(println, stan_file; append=true)
+        @test_throws ArgumentError StanProblem(post, path; nan_on_error=nan_on_error)
+
+        # Force re-copying Stan file.
+        prob3 = StanProblem(post, path; force=true, nan_on_error=nan_on_error)
+        LogDensityProblems.logdensity(prob3, x)
     end
 end
